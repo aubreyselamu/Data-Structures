f017ed380ac50572758c90f8a6eccb32d21db344
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-12-21 08:22:31 -0500

    implemented insert and remove methods

--- Linked_Lists/doubly_linked_list.js
+++ Linked_Lists/doubly_linked_list.js
@@ -87,5 +87,45 @@ class DoublyLinkedList{
         }
         return current;
     }
-    set(index, val)
+    set(index, val){
+        foundNode = this.get(index)
+        if(foundNode !== null){
+            foundNode.val = val;
+            return true
+        } 
+        return false;
+    }
+    insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+        
+        let newNode = new Node(val);
+        let beforeNode = this.get(index -1);
+        let afterNode = beforeNode.next;
+        
+        beforeNode.next = newNode, newNode.prev = beforeNode;
+        newNode.next = afterNode, afterNode.prev = newNode;
+        this.length++;
+        return true;       
+    } 
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift()
+        if(index === this.length-1) return this.pop()
+
+        let removedNode = this.get(index)
+        let beforeNode = removedNode.prev
+        let afterNode = removedNode.next
+
+        beforeNode.next = afterNode
+        afterNode.prev = beforeNode
+
+        removedNode.next = null;
+        removedNode.prev = null;
+
+        this.length--;
+        return removedNode
+        
+    }
 }
