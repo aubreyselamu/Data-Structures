29451dd0bd1913527de40092ce0585f9738c1663
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2022-03-02 20:01:48 -0500

    implemented function to extract max

--- BinaryHeaps.js
+++ BinaryHeaps.js
@@ -23,6 +23,48 @@ class MaxBinaryHeap {
     }
 
     extractMax(){
-        
+        let max = this.values[0]
+        const end = this.values.pop()
+        if(this.values.length > 0){
+            this.values[0] = end
+            this.sinkDown()
+        }
+        return max
+    }
+
+    sinkDown(){
+        // [33,39,41,18,27,12]
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.value[0];
+
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild > element){
+                    swap = leftChildIdx;
+                    element = this.values[swap]
+
+                }
+            }
+
+            if(rightChild < length){
+                rightChild = this.values[rightChildIdx]
+                if((swap === null && rightChild > element) || (swap !== null && rightChild > element)){
+                    swap = rightChildIdx;
+                }
+            }
+
+            if(swap === null) break;
+            this.values[idx] = swap;
+            this.values[swap] = element;
+            idx = swap;
+        }
+
     }
 }
